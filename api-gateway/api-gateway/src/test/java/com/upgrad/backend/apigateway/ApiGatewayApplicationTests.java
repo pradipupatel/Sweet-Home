1d03e625f03e0d0c99680d07b0b474aa47f3b9e3
shaleshk007 <shaleshk007@gmail.com>
2023-05-24 00:43:54 +0530

    'Commiting'

---
+++ api-gateway/api-gateway/src/test/java/com/upgrad/backend/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.backend.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
